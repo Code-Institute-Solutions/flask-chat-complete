7a39880ac426337ff887888a7c1d4289f1bf5e46
Matt Rudge <matt.rudge@codeinstitute.net>
2018-11-07 16:40:24 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,41 @@
+import os
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session
+
+app = Flask(__name__)
+app.secret_key = "randomsecretkey"
+messages = []
+
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    now=datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    
+    if "username" in session:
+        return redirect(session["username"])
+        
+    return render_template("index.html")
+
+
+@app.route('/<username>', methods=["GET", "POST"])
+def user(username):
+    """Display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(username)
+        
+    return render_template("chat.html",
+                            username=username, chat_messages=messages)
+
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
